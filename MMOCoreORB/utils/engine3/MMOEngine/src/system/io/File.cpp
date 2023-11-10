03b6565f0dbd80dc35d73e5586d246adc9efa45e
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2023-11-10 16:04:31 +0100

    Fix missing method in File

--- MMOCoreORB/utils/engine3/MMOEngine/src/system/io/File.cpp
+++ MMOCoreORB/utils/engine3/MMOEngine/src/system/io/File.cpp
@@ -175,6 +175,10 @@ bool File::mkpath(const String& path, int permissions) {
 	return (status);
 }
 
+const String File::getFileName() const {
+	return name;
+}
+
 const String File::getBaseName() const {
 	static std::mutex guard;
 	std::unique_lock<std::mutex> lock(guard);
